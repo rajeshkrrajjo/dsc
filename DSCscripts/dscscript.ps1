d42f79ec432548050c2c3c9763380ca3541efa97
Rajesh <https://functestaz.visualstudio.com/_git/Funcdeploy>
2017-12-06 02:32:01 +1400

    uodated code

--- DSCscripts/dscscript.ps1
+++ DSCscripts/dscscript.ps1
@@ -1,3 +1,31 @@
+Install-WindowsFeature -Name DSC-Service
+Configuration ContosoWebsite
+{
+  param ($MachineName)
+
+  Node $MachineName
+  {
+    #Install the IIS Role
+    WindowsFeature IIS
+    {
+      Ensure = “Present”
+      Name = “Web-Server”
+    }
+
+    #Install ASP.NET 4.5
+    WindowsFeature ASP
+    {
+      Ensure = “Present”
+      Name = “Web-Asp-Net45”
+    }
+
+     WindowsFeature WebServerManagementConsole
+    {
+        Name = "Web-Mgmt-Console"
+        Ensure = "Present"
+    }
+  }
+} 
 Save-Module -LiteralPath "C:\Program Files\WindowsPowerShell\Modules" -Name "xPSDesiredStateConfiguration"
 Save-Module -LiteralPath "C:\Program Files\WindowsPowerShell\Modules" -Name "xDSCFirewall"
 Save-Module -LiteralPath "C:\Program Files\WindowsPowerShell\Modules" -Name "psdesiredstateconfiguration"
